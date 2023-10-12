cb0fb13cb8ba7c96a41996c5ea7f9bee7b07faa7
Prashik Maghade <139534949+Prashik109@users.noreply.github.com>
2023-10-13 01:15:46 +0530

    Create linkedlist(a).cpp

---
+++ linkedlist(a).cpp
@@ -0,0 +1,26 @@
+//PRASHIKMAGHADE
+//Prn 22070123109
+#include<iostream>
+using namespace std;
+class Node
+{
+    public:
+    int val;
+    Node*next;
+
+    Node(int data)
+    {
+        val=data;
+        next=NULL;
+    }
+};
+int main()
+{
+    Node*n=new Node(1);
+    cout<<n->val<<" "<<n->next<<endl;
+    return 0;
+}
+/*
+Output:
+1 0
+*/
